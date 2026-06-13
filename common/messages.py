525e7616f769a815c1ae62bafdbe3d9ef7b8111a
moccbc <yutausa2010@gmail.com>
2026-06-13 11:47:39 -0700

    Update messages

--- common/messages.py
+++ common/messages.py
@@ -7,7 +7,6 @@ MESSAGES = {}
 
 def message(tag):
     def wrap(clazz):
-        print(clazz)
         clazz.type = tag
         MESSAGES[tag] = clazz
         return clazz
@@ -25,25 +24,37 @@ class Move:
     player_id: int
     y: int
 
-# Goes from server -> client
-@message("game_ready")
-@dataclass
-class GameReady:
-    pass
-
 @message("player_move")
 @dataclass
 class PlayerMove:
     player_id: int
     dy: float
 
-@message("ball_move")
+# Goes from server -> client
+@message("join_response")
 @dataclass
-class BallMove:
+class JoinResponse:
     player_id: int
-    dy: float
 
-Message = Join | Move | GameReady | PlayerMove | BallMove
+@message("game_ready")
+@dataclass
+class GameReady:
+    pass
+
+@message("paddle_position")
+@dataclass(frozen=True)
+class PaddlePosition:
+    player_id: int
+    x: float
+    y: float
+
+@message("ball_position")
+@dataclass(frozen=True)
+class BallPosition:
+    x: float
+    y: float
+
+Message = Join | JoinResponse | Move | GameReady | PlayerMove | PaddlePosition | BallPosition
 
 def encode(msg: Message): 
     return json.dumps({"type": msg.type, **asdict(msg)})
